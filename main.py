ba93df8516dfb40cee828553bc86faa4184bdbdd
Nikhil Bola Kamath <nikhilkb98@gmail.com>
2024-09-20 22:14:34 -0700

    Update main

--- main.py
+++ main.py
@@ -293,29 +293,6 @@ def generate_route(
 
 # -----------------------------------------------------------------------------------------------------------------------------
 
-# ------------------------------------------------- Waymo -----------------------------------------------------------------
-@__waymo_app__.command(name="visualize_waymo_open_motion_data", help="This command visualizes the Waymo open motion dataset.")
-def visualize_waymo_open_motion_data(
-    input_directory: Optional[str] = T.Option(
-        "./data/online/waymo/waymo_open_dataset_motion_v_1_2_1/uncompressed/tf_example", help="The directory containing the Waymo open motion dataset."),
-    output_directory: Optional[str] = T.Option(
-        "./data/interim", help="The directory where the visualization will be stored."),
-    scenario_id: Optional[str] = T.Option(
-        "training_tfexample.tfrecord-00000-of-01000", help="The scenario id for the Waymo open motion dataset."),
-    output_filename: Optional[str] = T.Option(
-        None, help="The name of the output file with extension."),
-) -> None:
-    # Print the configuration of this function.
-    print_param_table(
-        parmas=locals(), title="Parameters for `visualize_waymo_open_motion_data(...)`")
-    try:
-        # Instantiate and configure the Waymo open motion dataset instance.
-        print("TODO: Implement this command.")
-    except Exception as e:
-        logger.error(
-            f"An error occurred while visualizing the Waymo open motion data: {e}")
-        raise e
-
 # ------------------------------------------------- AGROVERSE -----------------------------------------------------------------
 @__agroverse_app__.command(name="visualize_agroverse_data", help="This command visualizes the Agroverse forecasting data.")
 def visualize_agroverse_forecasting_data(
@@ -423,6 +400,31 @@ def av2_forecasting_query_max_occurrence(
             f"An error occurred while quering the analytics file for the Agroverse forecasting data: {e}")
         raise e
     
+# -----------------------------------------------------------------------------------------------------------------------------
+
+# ------------------------------------------------- Waymo ---------------------------------------------------------------------
+@__waymo_app__.command(name="visualize_waymo_open_motion_data", help="This command visualizes the Waymo open motion dataset.")
+def visualize_waymo_open_motion_data(
+    input_directory: Optional[str] = T.Option(
+        "./data/online/waymo/waymo_open_dataset_motion_v_1_2_1/uncompressed/tf_example", help="The directory containing the Waymo open motion dataset."),
+    output_directory: Optional[str] = T.Option(
+        "./data/interim", help="The directory where the visualization will be stored."),
+    scenario_id: Optional[str] = T.Option(
+        "training_tfexample.tfrecord-00000-of-01000", help="The scenario id for the Waymo open motion dataset."),
+    output_filename: Optional[str] = T.Option(
+        None, help="The name of the output file with extension."),
+) -> None:
+    # Print the configuration of this function.
+    print_param_table(
+        parmas=locals(), title="Parameters for `visualize_waymo_open_motion_data(...)`")
+    try:
+        # Instantiate and configure the Waymo open motion dataset instance.
+        print("TODO: Implement this command.")
+    except Exception as e:
+        logger.error(
+            f"An error occurred while visualizing the Waymo open motion data: {e}")
+        raise e
+
 # -----------------------------------------------------------------------------------------------------------------------------
 
 # -----------------------------------------------  MODULE TEST  ---------------------------------------------------------------
